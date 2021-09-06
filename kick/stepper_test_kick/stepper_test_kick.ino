5ed837be843c8bdd783bcfb855152bb2b0e5e9f5
aanper <mail@s3f.ru>
2021-09-06 19:17:01 +0300

    adapt to cyland board

--- kick/stepper_test_kick/stepper_test_kick.ino
+++ kick/stepper_test_kick/stepper_test_kick.ino
@@ -1,76 +1,55 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
+const int stp =  A5;
+const int en =  A0;
+const int dir =  A1;
 
-  This example code is in the public domain.
-*/
+const int pullup = A4;
 
-const int stp1 =  5;
-const int stp2 =  6;
-const int stp3 =  7;
+int ledState = LOW;
+int dirState = LOW;
 
-const int en1 =  8; //was 9
-
-const int dir1 =  2;
-const int dir2 =  3;
-const int dir3 =  4;
-
-
-int ledState, ledState2, ledState3 = LOW;
-int dirState, dirState2, dirState3 = LOW;
+const int led = 13;
 
 int maxcnt = 1300; //1800 2100 1000
 int back =  50; //1800 2100
 
 int del = 20; //50 40 20 14
 
-int but = 0;
+const int BUTTON_PIN = 9;
 
-#define BUTTON_PIN 11
-
-// the setup routine runs once when you press reset:
 void setup() {
   // initialize the digital pin as an output.
-  //  pinMode(led, OUTPUT);
-
-  digitalWrite(dir1, LOW);
-  digitalWrite(dir2, LOW);
-  digitalWrite(dir3, LOW);
-
-  pinMode(stp1, OUTPUT);
-  for (int i = 2; i <= 10; i++) {
-    pinMode(i, OUTPUT);
-    digitalWrite(i, HIGH);
-  }
-  Serial.begin(115200);
-  digitalWrite(en1, HIGH);
-  //delay(2000);
-
-  digitalWrite(en1, HIGH);
-
-
-
+  pinMode(led, OUTPUT);
+  pinMode(stp, OUTPUT);
+  pinMode(en, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-
-
+  pinMode(pullup, OUTPUT);
+  digitalWrite(pullup, HIGH);
+  
+  
+  digitalWrite(dir, LOW);
+  
+  Serial.begin(115200);
+  digitalWrite(en, HIGH);
+  //delay(2000);
 }
 
 
 
 // the loop routine runs over and over again forever:
 void loop() {
-  but = digitalRead(11);
+  int but = digitalRead(BUTTON_PIN);
 
-  if (but == LOW) {
-    digitalWrite(en1, HIGH);
+  if (but == HIGH) {
+    digitalWrite(led, HIGH);
+    digitalWrite(en, HIGH);
     delay(50);
-    digitalWrite(dir3, HIGH);
+    digitalWrite(dir, HIGH);
     delay(10);
 
     for (int i = 0; i <= maxcnt; i++) {
-      digitalWrite(stp3, HIGH);
+      digitalWrite(stp, HIGH);
       delayMicroseconds(del);               // wait for a second
-      digitalWrite(stp3, LOW);
+      digitalWrite(stp, LOW);
       delayMicroseconds(30);               // wait for a second
     }
 //    digitalWrite(dir3, LOW);
@@ -84,8 +63,8 @@ void loop() {
 //    }
     //delay (4);
 
-    digitalWrite(en1, LOW);
+    digitalWrite(en, LOW);
     delay(30);
-
+    digitalWrite(led, LOW);
   }
 }
